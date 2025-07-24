756fb068c3900dca086d6d165f4a78fdf6a2babc
Cupillard Emilie <TRNRMF@ad.insee.intra>
2025-07-24 18:30:48 +0200

    chemin des donnees dans un endroit accessible pour toute la DESE

--- R/get_data.R
+++ R/get_data.R
@@ -1,3 +1,3 @@
 
-file.copy(from = "N:/GDCJ/N-GDCJ/Echanges.DCJ/DSC/Formations/Bonnes pratiques Git R/individu_reg.csv",
+file.copy(from = "N:/GDESE/O-GDESE/Echanges/DCJ/DSC/Bonnes pratiques Git R/individu_reg.csv",
           to = file.path(getwd(), "individu_reg.csv") )
